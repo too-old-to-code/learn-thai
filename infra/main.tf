4ba00569b15a8aa29a46800ded9ee8ee7b3377ff
Andrew Trigg <andrewt51@hotmail.com>
2021-06-22 22:15:45 +0100

    Add script

---
+++ infra/main.tf
@@ -0,0 +1,53 @@
+
+# Declare which provider we are using
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# Set a variable for the name of our bucket
+variable "bucket_name" {
+  default = "learn-thai"
+}
+
+# Create a bucket, complete with bucket policy
+resource "aws_s3_bucket" "react_bucket" {
+  bucket = "${var.bucket_name}"
+  acl    = "public-read"
+  force_destroy = true
+
+  policy = <<EOF
+{
+  "Id": "bucket_policy_site",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "bucket_policy_site_main",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.bucket_name}/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+output "website_domain" {
+  value = "${aws_s3_bucket.react_bucket.website_domain}"
+}
+
+output "website_endpoint" {
+  value = "${aws_s3_bucket.react_bucket.website_endpoint}"
+}
+
+output "bucket_name" {
+  value = "${var.bucket_name}"
+}
+
---
+++ infra/scripts/buildAndPublish.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Set some variables
+SOURCE_DIR="../frontend"
+GIT_COMMIT_HASH='git rev-parse --short HEAD'
+BUCKET_NAME='learn-thai'
+TARGET_DIR="$BUCKET_NAME/$GIT_COMMIT_HASH/learn-thai-alphabet"
+echo "$TARGET_DIR"
+
+# Remove the existing build
+rm -rf "$SOURCE_DIR/build"
+
+# Install node modules and creat new build
+(cd "$SOURCE_DIR" && npm ci && npm run build)
+
+# Copy the build application to the deployed S3 bucket
+aws s3 sync "$SOURCE_DIR/build" "s3://$TARGET_DIR/"
