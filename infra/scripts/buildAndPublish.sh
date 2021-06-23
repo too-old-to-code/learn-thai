cc81804b339666a054e810f6623c46c58826721b
Andrew Trigg <andrewt51@hotmail.com>
2021-06-23 19:27:02 +0100

    Trying still

--- infra/scripts/buildAndPublish.sh
+++ infra/scripts/buildAndPublish.sh
@@ -1,24 +1,19 @@
 #! /bin/sh
 
-set -e
-
 # Set some variables
-# SOURCE_DIR="."
-GIT_COMMIT_HASH=`git rev-parse --short HEAD`
-BUCKET_NAME='learn-thaistuff'
+SOURCE_DIR="./frontend"
+GIT_COMMIT_HASH='git rev-parse --short HEAD'
+BUCKET_NAME='learn-thai'
 TARGET_DIR="$BUCKET_NAME/$GIT_COMMIT_HASH/learn-thai-alphabet"
 echo "$TARGET_DIR"
-echo 'why oh why oh why'
-echo $(ls)
-# Remove the existing build
-# rm -rf "$SOURCE_DIR/build"
 
-# cd "$SOURCE_DIR"
-echo =================================
-echo $(ls)
+# Remove the existing build
+rm -rf "$SOURCE_DIR/build"
 
 # Install node modules and creat new build
-(npm i && npm run build)
+(cd "$SOURCE_DIR" && npm ci && npm run build)
+
+echo $(ls)
 
 # Copy the build application to the deployed S3 bucket
-aws s3 sync "build" "s3://$TARGET_DIR/"
+aws s3 sync "$SOURCE_DIR/build" "s3://$TARGET_DIR/"
